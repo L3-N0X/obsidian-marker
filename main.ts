ecb17f437ac56ff2cbd2234e7cda6841fc834900
Leon Gött <leon.goett@web.de>
2024-07-23 18:16:47 +0200

    Initial setup for obsidian-marker

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface MarkerSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: MarkerSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Marker extends Plugin {
+	settings: MarkerSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: Marker;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Marker) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
